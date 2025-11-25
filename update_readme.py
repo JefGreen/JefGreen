8532f56cf4d16e9b4ef9abe61409f77579b468b7
JefGreen <trempe.jeff@gmail.com>
2025-11-25 07:39:22 -0500

    wip

--- update_readme.py
+++ update_readme.py
@@ -9,12 +9,12 @@ def update_readme_date():
 
 
     # TODO: use a regex to match the url what ever the date is and replace it with the current date and time
-    new_content = content.replace(
-        <img src=f"https://tryhackme-images.s3.amazonaws.com/streak/Some1ShouldPatchThis.png#{current_date}" alt="Streak">
-    )
+    # new_content = content.replace(
+    #     <img src=f"https://tryhackme-images.s3.amazonaws.com/streak/Some1ShouldPatchThis.png#{current_date}" alt="Streak">
+    # )
 
-    with open(readme_path, "w") as f:
-        f.write(new_content)
+    # with open(readme_path, "w") as f:
+    #     f.write(new_content)
 
 if __name__ == "__main__":
     update_readme_date()
